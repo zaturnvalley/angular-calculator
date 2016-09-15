864408c300bb0005dd93f1eb5ea0685cb36e8654
zaturnvalley <zacharycmason@gmail.com>
2016-09-15 14:36:21 -0700

    used angular to display numbers, bind together. still needs operators

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-console.log('app.js was loaded.');
+var CalcApp = angular.module('CalcApp', []);
+
+CalcApp.controller('CalcCtrl', ['$scope', function($scope) {
+  $scope.display = '0';
+
+  var numArray = [];
+  var storedNum = [];
+
+  $scope.toggleVal = function(input) {
+    numArray.push(input);
+    joinedArray = numArray.join("")
+    $scope.display = joinedArray;
+    console.log(numArray.join(""));
+  }
+
+  $scope.delete = function(){
+    numArray = [];
+    $scope.display = '0';
+  }
+
+  $scope.div = function(){
+    storedNum.push(numArray);
+    numArray = [];
+
+  }
+
+  $scope.mult = function(){
+    storedNum.push(numArray);
+    numArray = [];
+
+  }
+
+  $scope.add = function(){
+    storedNum.push(numArray);
+    numArray = [];
+
+  }
+
+  $scope.sub = function(){
+    storedNum.push(numArray);
+    numArray = [];
+
+  }
+}])
